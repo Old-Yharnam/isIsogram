59b31e4c7025165adc9b317db07454648346aad3
Old-Yharnam <arianrupp.2016@gmail.com>
2026-05-07 11:32:55 +0000

    New Code!!

--- test/test_isogram.c
+++ test/test_isogram.c
@@ -3,6 +3,28 @@
 #include "vendor/unity.h"
 #include <stdlib.h>
 
+bool is_isogram(const char phase[]){
+
+    int freq[255] = {0}
+    int i = 0;
+
+    while (phase[i] != '\0'){
+        freq[i] ++;
+        i++
+    }
+
+    int max = 0;
+    for (int j = 0; j < 255; j++){
+        if (freq[j] != 0){
+            if (max == 0) max = freq[j];
+            else if (freq[j] != max) return false;)
+        }
+    }
+    return true;
+
+}
+
+
 void setUp(){}
 
 void tearDown(){}
